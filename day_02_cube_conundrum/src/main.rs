a2593783c8b954a6eddc5e145796b2c63b27322b
Rene Moll <github@r-moll.nl>
2024-01-03 08:54:23 +0100

    day 02, part 1

---
+++ day_02_cube_conundrum/src/main.rs
@@ -0,0 +1,70 @@
+use std::fs;
+
+#[derive(Debug)]
+struct Game {
+    red: usize,
+    blue: usize,
+    green: usize
+}
+
+impl Game {
+    fn valid(&self, other: &Game) -> bool {
+        return self.red <= other.red
+            && self.blue <= other.blue
+            && self.green <= other.green;
+    }
+}
+
+fn game_from_str(game: &str) -> Game {
+    let mut g = Game{red: 0, blue: 0, green: 0};
+    for dice in game.split(",") {
+        let mut x = dice.trim().split(" ");
+        let n = x.next().unwrap().parse().unwrap();
+
+        if dice.find("red").is_some() {
+            g.red = n;
+        }
+        if dice.find("blue").is_some() {
+            g.blue = n;
+        }
+        if dice.find("green").is_some() {
+            g.green = n;
+        }
+    }
+    g
+}
+
+fn do_part1(contents: &String) -> usize {
+    let max = Game {
+        red: 12,
+        blue: 14,
+        green: 13
+    };
+
+    let mut sum = 0;
+
+    for line in contents.trim().lines() {
+        let mut it = line.split(':');
+        let id: usize = it.next().unwrap().split(" ").last().unwrap().parse().unwrap();
+
+        let draws = it.last().unwrap().split(";");
+        let mut valid = true;
+        for draw in draws {
+            valid = valid && game_from_str(draw).valid(&max);
+        }
+
+        if valid {
+            sum += id;
+        }
+    }
+    sum
+}
+
+fn main() {
+    let file_path = "input.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let part1 = do_part1(&contents);
+    println!("Result part 1: {part1}");
+    assert!(part1 == 2600);
+}
