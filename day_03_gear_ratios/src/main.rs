ea336e148641654fb01be519096c93edf9d8cffb
Rene Moll <github@r-moll.nl>
2024-01-05 16:16:53 +0100

    day 03, part 2

--- day_03_gear_ratios/src/main.rs
+++ day_03_gear_ratios/src/main.rs
@@ -12,7 +12,7 @@ impl Matrix<u8> {
         let data: Vec<_> = input.lines().map(str::as_bytes).collect();
         let rows = data.len();
         let columns = data[0].len();
-        Matrix {rows, columns, data: data.concat()}
+        Matrix{rows, columns, data: data.concat()}
     }
 }
 
@@ -26,11 +26,17 @@ impl<T> Matrix<T> {
     }
 }
 
-fn do_part1(contents: &String) -> usize {
+struct Input {
+    indices: Matrix<usize>,
+    numbers: Vec<usize>,
+    symbols: Vec<(isize, isize, u8)>
+}
+
+fn parse(contents: &String) -> Input {
     let data = Matrix::parse(contents);
     let mut indices = Matrix {rows: data.rows, columns: data.columns, data: vec![0; data.rows * data.columns]};
     let mut numbers = vec![0];
-    let mut symbols: Vec<(isize, isize)> = Vec::new();
+    let mut symbols: Vec<(isize, isize, u8)> = Vec::new();
 
     for r in 0..data.rows {
         let mut num = 0;
@@ -46,7 +52,7 @@ fn do_part1(contents: &String) -> usize {
             }
 
             if !el.is_ascii_digit() && *el != b'.' {
-                symbols.push((r.try_into().unwrap(), c.try_into().unwrap()));
+                symbols.push((r.try_into().unwrap(), c.try_into().unwrap(), *el));
             }
         }
 
@@ -55,9 +61,15 @@ fn do_part1(contents: &String) -> usize {
         }
     }
 
+    Input{indices, numbers, symbols}
+}
+
+fn do_part1(input: &Input) -> usize {
+    let Input{indices, numbers, symbols} = input;
+
     let mut result = 0;
     let diag = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)];
-    for (r, c) in symbols {
+    for (r, c, _) in symbols {
         let mut processed: Vec<usize> = Vec::new();
         for (dr, dc) in diag {
             let x = (r + dr) as usize;
@@ -73,19 +85,45 @@ fn do_part1(contents: &String) -> usize {
     result
 }
 
-fn do_part2(contents: &String) -> usize {
-    0
+fn do_part2(input: &Input) -> usize {
+    let Input{indices, numbers, symbols} = input;
+
+    let mut result = 0;
+    let diag = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)];
+    for (r, c, s) in symbols {
+        if *s != b'*' {
+            continue;
+        }
+
+        let mut ratio = 1;
+        let mut processed: Vec<usize> = Vec::new();
+        for (dr, dc) in diag {
+            let x = (r + dr) as usize;
+            let y = (c + dc) as usize;
+            let idx = *indices.get(x, y);
+            if idx > 0 && !processed.contains(&idx) {
+                ratio = ratio * numbers[idx];
+                processed.push(idx);
+            }
+        }
+        if processed.len() > 1 {
+            result += ratio;
+        }
+    }
+
+    result
 }
 
 fn main() {
     let file_path = "input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let input = parse(&contents);
 
-    let part1 = do_part1(&contents);
+    let part1 = do_part1(&input);
     println!("Result part 1: {part1}");
-    // assert!(part1 == 546563);
+    assert!(part1 == 546563);
 
-    let part2 = do_part2(&contents);
+    let part2 = do_part2(&input);
     println!("Result part 2: {part2}");
-    // assert!(part2 == );
+    assert!(part2 == 91031374);
 }
