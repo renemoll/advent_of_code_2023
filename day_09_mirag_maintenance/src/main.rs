77ff5cdff0b5ec2f43844de8ade2b637a38a1fce
Rene Moll <github@r-moll.nl>
2024-01-16 22:07:08 +0100

    day 09: reduce sequence iteration with a function parameter

--- day_09_mirag_maintenance/src/main.rs
+++ day_09_mirag_maintenance/src/main.rs
@@ -59,15 +59,6 @@ fn process_forwards(xs: &Sequence) -> isize {
     return xs.data.last().unwrap() + process_forwards(&d);
 }
 
-fn do_part1(input: &Input) -> isize {
-    let mut result = 0;
-    for s in input.sequences.iter() {
-        result += process_forwards(s);
-    }
-
-    result
-}
-
 fn process_backwards(xs: &Sequence) -> isize {
     if xs.all_zero() {
         return 0;
@@ -77,10 +68,10 @@ fn process_backwards(xs: &Sequence) -> isize {
     return xs.data.first().unwrap() - process_backwards(&d);
 }
 
-fn do_part2(input: &Input) -> isize {
+fn do_part(input: &Input, f: &dyn Fn(&Sequence) -> isize) -> isize {
     let mut result = 0;
     for s in input.sequences.iter() {
-        result += process_backwards(s);
+        result += f(s);
     }
 
     result
@@ -91,11 +82,11 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let input = parse(&contents);
 
-    let part1 = do_part1(&input);
+    let part1 = do_part(&input, &process_forwards);
     println!("Result part 1: {part1}");
     assert!(part1 == 1987402313);
 
-    let part2 = do_part2(&input);
+    let part2 = do_part(&input, &process_backwards);
     println!("Result part 2: {part2}");
     assert!(part2 == 900);
 }
