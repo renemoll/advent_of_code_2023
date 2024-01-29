9f965489891e5083b61601c0ae51dfc64c19c4b5
Rene Moll <github@r-moll.nl>
2024-01-29 21:43:33 +0100

    day 12, attempt 1

--- day_12_hot_springs/src/main.rs
+++ day_12_hot_springs/src/main.rs
@@ -1,34 +1,52 @@
 use std::fs;
 use std::iter::zip;
 
-#[derive(Debug)]
-struct Input<'a> {
-    sections: Vec<&'a str>,
-    lengths: Vec<usize>,
+// #[derive(Debug)]
+// struct Input<'a> {
+    // sections: Vec<&'a str>,
+    // lengths: Vec<usize>,
+// }
+
+struct Input {
+    pattern: &str,
+    length: Vec<usize>
 }
 
 fn parse(input: &str) -> Vec<Input> {
     let mut result: Vec<Input> = Vec::new();
 
-    for raw in input.lines() {
-        let l = raw.split(' ').collect::<Vec<_>>();
-        let mut sections = l[0].split('.')
-            .filter(|&x| x.len() > 0)
-            .collect::<Vec<_>>();
-        let mut numbers: Vec<usize> = l[1].split(',').map(|x| x.parse().unwrap()).collect::<Vec<_>>();
+    for line in input.lines() {
+        let l = line.split(' ').collect::<Vec<_>>();
 
-        let mut offset = 0;
-        for (n, &s) in sections.clone().iter().enumerate() {
-            let length = s.chars().filter(|&x| x == '#').count();
-            if length == s.len() {
-                let index_num = numbers.iter().position(|&x| x == length).unwrap();
-                numbers.remove(index_num);
-                sections.remove(n - offset);
-                offset += 1;
+        result.push(
+            Input {
+                pattern: l[0],
+                length: l[1].split(',')
+                    .map(|x| x.parse().unwrap())
+                    .collect::<Vec<_>>()
             }
-        }
-        result.push(Input{sections: sections, lengths: numbers});
+        )
     }
+
+    // for raw in input.lines() {
+        // let l = raw.split(' ').collect::<Vec<_>>();
+        // let mut sections = l[0].split('.')
+        //     .filter(|&x| x.len() > 0)
+        //     .collect::<Vec<_>>();
+        // let mut numbers: Vec<usize> = l[1].split(',').map(|x| x.parse().unwrap()).collect::<Vec<_>>();
+
+        // let mut offset = 0;
+        // for (n, &s) in sections.clone().iter().enumerate() {
+        //     let length = s.chars().filter(|&x| x == '#').count();
+        //     if length == s.len() {
+        //         let index_num = numbers.iter().position(|&x| x == length).unwrap();
+        //         numbers.remove(index_num);
+        //         sections.remove(n - offset);
+        //         offset += 1;
+        //     }
+        // }
+        // result.push(Input{sections: sections, lengths: numbers});
+    // }
 
     result
 }
@@ -37,17 +55,17 @@ fn do_part1(input: &Vec<Input>) -> usize {
     println!("{:?}", input);
 
     let mut result = 0;
-    for i in 0..input.len() {
-        if input[i].sections.len() == input[i].lengths.len() {
-            // number of sections match the numbers of numbers
-            for (s, n) in zip(input[i].sections.iter(), input[i].lengths.iter()) {
-                println!("- section: {:?}, length: {:?}, result: {:?}", s, n, s.len() / n);
-                result += s.len() / n;
-            }
-        } else {
-            println!("= section: {:?}, length: {:?}, result: {:?}", input[i].sections, input[i].lengths, 0);
-        }
-    }
+    // for i in 0..input.len() {
+    //     if input[i].sections.len() == input[i].lengths.len() {
+    //         // number of sections match the numbers of numbers
+    //         for (s, n) in zip(input[i].sections.iter(), input[i].lengths.iter()) {
+    //             println!("- section: {:?}, length: {:?}, result: {:?}", s, n, s.len() / n);
+    //             result += s.len() / n;
+    //         }
+    //     } else {
+    //         println!("= section: {:?}, length: {:?}, result: {:?}", input[i].sections, input[i].lengths, 0);
+    //     }
+    // }
 
     result
 }
